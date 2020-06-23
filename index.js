b1e8d929f4b8e43d172b0fe618db6d3768ed784e
Bob Whitmer <rjwhitmercc@gmail.com>
2020-06-23 16:05:50 -0600

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,28 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(timeString){
+  let splitTime = timeString.split(":")
+
+  console.log(splitTime[0])
+  if (splitTime[0] < 12) {
+    return ("Good Morning")
+  }
+  if (12 <= splitTime[0] && splitTime[0] <= 17) {
+    return ("Good Afternoon")
+  }
+  if (splitTime[0] > 17) {
+    return ("Good Evening")
+  }
+  // console.log(timeString.split(":"))
+}
 /* Write your implementation of displayMessage() */
+
+function displayMessage(string) {
+  let greeting = document.getElementById('greeting')
+  let body = document.querySelector('body')
+  greeting.innerText = string
+  console.log(greeting)
+  body.appendChild(greeting)
+}
